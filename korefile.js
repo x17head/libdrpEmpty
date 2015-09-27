f9f5227e1fc97841ab0f199b0090d7011c32daf9
x17head <x.1.7.head@gmail.com>
2015-09-27 00:59:27 -0400

    Delete korefile.js
    oops

--- korefile.js
+++
@@ -1,8 +0,0 @@
-var solution = new Solution('LibdrpEmpty');
-var project = new Project('LibdrpEmpty');
-project.setDebugDir('build/windows');
-project.addSubProject(Solution.createProject('build/windows-build'));
-project.addSubProject(Solution.createProject('C:/HaxeToolkit/haxe/lib/kha/15,8,0'));
-project.addSubProject(Solution.createProject('C:/HaxeToolkit/haxe/lib/kha/15,8,0/Kore'));
-solution.addProject(project);
-return solution;
